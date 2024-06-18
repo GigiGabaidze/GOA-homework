5d32fdb625ef086324f34fd5b784f7b1557dfc2c
GigiGabaidze <gigigabaidze85@gmail.com>
2024-06-18 17:06:58 +0400

    commit

---
+++ level07/homework/07.py
@@ -0,0 +1,40 @@
+name = input("what is your name: ")
+last_name = input("what is your last name: ")
+age = input("what is your age: ")
+country = input("which country do you live" )
+
+print("my name is ", name, last_name, "i am ", age, " years old" " and i live in ", country)
+
+
+#მომხმარებელს შემოატანინეთ 3 რიცხვი და ამ 3 რიცხვზე გააკეთეთ 15 მაგალითი
+
+
+number90 = int(input("90--->:"))
+number50 = int(input("50--->:"))
+number30 = int(input("30--->:"))
+
+print(number90 + number30)
+print(number50 + number30)
+print(number90 - number50)
+print(number30 * number90 )
+print(number90 / number50)
+print(number90 + number50 + number90)
+print(number50 * number90)
+print(number90 / number30)
+print(number30 * number90)
+print(number50 + number90 )
+print(number90 / number30)
+print(number90 * number30 / number50)
+print(number50 * number30 + number90)
+print(number30 + number90 * number50)
+print(number50 / number90)
+
+
+#ორ მომხმარებელს შორის ააგე დიალოგი
+
+g = input("me: ")
+print("Goga: gamarjoba ")
+g = input("me: ")
+print("goga: 10 ")
+g = input("me: ")
+print("goga: tbilisi")
